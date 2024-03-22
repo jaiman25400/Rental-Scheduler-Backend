8e56c54eb03639e5aa7c15bc996821a5974df8f2
Jaiman Joshi <jaimanjoshi25@gmail.com>
2024-03-22 01:53:13 -0400

    Created Multiple File Upload Logic

--- src/rentals/rentals.controller.ts
+++ src/rentals/rentals.controller.ts
@@ -10,7 +10,7 @@ import {
 import { RentalsService } from './rentals.service';
 import { Rentals } from './rentals.interface';
 import { Express } from 'express';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import * as path from 'path';
 import { rentals } from './rentals.mock';
@@ -25,9 +25,43 @@ export class RentalsController {
     return this.rentalServices.getProperties();
   }
 
+  // @Post()
+  // @UseInterceptors(
+  //   FileInterceptor('file', {
+  //     storage: diskStorage({
+  //       destination: './uploads',
+  //       filename: (req, files, cb) => {
+  //         cb(null, `${files.originalname}`);
+  //       },
+  //     }),
+  //   }),
+  // )
+  // public postRentals(
+  //   @Body() rental: Rentals,
+  //   @UploadedFile() file: Express.Multer.File,
+  // ) {
+  //   console.log('API Hit::: ', rental);
+
+  //   const fileLink = `http://localhost:3000/${file.originalname}`;
+  //   console.log('File Link: ', fileLink);
+  //   // Include the file link in the rental object
+  //   if (!rental.images) {
+  //     rental.images = []; // Initialize the array if it's not already initialized
+  //   }
+  //   rental.images.push(fileLink); // Push the file link to the images array
+
+  //   const mockDataLength = rentals.length;
+  //   rental.id = (mockDataLength + 1).toString();
+
+  //   // Add the current date
+  //   rental.cleaningDate = new Date();
+  //   console.log('Rental :', rental);
+  //   return this.rentalServices.postProperties(rental);
+  // }
+
   @Post()
   @UseInterceptors(
-    FileInterceptor('file', {
+    FilesInterceptor('files', 10, {
       storage: diskStorage({
         destination: './uploads',
         filename: (req, files, cb) => {
@@ -38,17 +72,24 @@ export class RentalsController {
   )
   public postRentals(
     @Body() rental: Rentals,
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFiles() files: Array<Express.Multer.File>,
   ) {
     console.log('API Hit::: ', rental);
 
-    const fileLink = `http://localhost:3000/${file.originalname}`;
-    console.log('File Link: ', fileLink);
-    // Include the file link in the rental object
+    const fileLinks: string[] = []; // Array to store file links
+
+    // Process each file
+    files.forEach((file) => {
+      const fileLink = `http://localhost:3000/${file.originalname}`;
+      console.log('File Link: ', fileLink);
+      fileLinks.push(fileLink); // Push the file link to the array
+    });
+
+    // Include the file links in the rental object
     if (!rental.images) {
       rental.images = []; // Initialize the array if it's not already initialized
     }
-    rental.images.push(fileLink); // Push the file link to the images array
+    rental.images.push(...fileLinks); // Push the file links to the images array
 
     const mockDataLength = rentals.length;
     rental.id = (mockDataLength + 1).toString();
